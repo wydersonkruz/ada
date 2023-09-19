36202eae0f993b4a52895fcdbf92ac9e44f8a5a8
wydersonkruz <wydrap9@gmail.com>
2023-09-19 12:48:30 +0200

    creating own type in Generic Holder

---
+++ src/com/company/GenericHolder.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class GenericHolder<T> {
+    private T a = 5;
+
+    public T getA() {
+        return a;
+    }
+
+    public void setA(T a) {
+        this.a = a;
+    }
+}
